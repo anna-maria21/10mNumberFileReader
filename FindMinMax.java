c302149576bf82460b346f601c64090350d10ae9
Anna-Mariia Strelchenko <81164511+anna-maria21@users.noreply.github.com>
2022-06-21 12:39:55 +0300

    Update and rename FindMin.java to FindMinMax.java

--- FindMinMax.java
+++ FindMinMax.java
@@ -3,13 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 
-public class FindMin implements Runnable{
+public class FindMinMax implements Runnable{
     LineNumberReader lineNumberReader = null;
     FileReader fileReader = null;
     private long min = Long.MAX_VALUE;
+    private long max = Long.MIN_VALUE;
 
     public long getMin() {
         return min;
+    }
+    
+    public long getMax() {
+        return max;
     }
 
     public FindMin(String path) throws FileNotFoundException {
@@ -24,6 +29,7 @@ public class FindMin implements Runnable{
 //            System.out.println(curThread.getName() + " " + str);
             long longVal = Long.parseLong(str);
             if (longVal < min) min = longVal;
+            if (longVal < max) max = longVal;
         }
     }
 
